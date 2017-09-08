fe39e90ac5f549be7b5b6101cea1bf7ee8854c7a
knva <txt_ch@163.com>
2017-09-08 09:39:41 +0800

    up

--- shutdown.py
+++ shutdown.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
-''' 
+''' 注释
 Filename: "shutdown.py"
 author:   "knva"
 date  :   "2017-03-16"
 version:  "1.00"
 '''
 import os
-import sys
-import time
-from time import strftime, localtime
+import time,datetime
+import psutil
+dt = datetime.datetime.fromtimestamp(psutil.boot_time())
+startHour = dt.strftime("%H")
+startMin = dt.strftime("%M")
+byebyetime = "18:00:00"
+if(int(startHour)<9 and int(startMin)<40):
+    byebyetime = "18:00:00"
+else:
+    byebyetime = "18:30:00"
+nowTime =  time.strftime('%H:%M:%S',time.localtime(time.time()))
 
-Closingtime = "18:00:00";
-nowTime =  time.strftime('%H:%M:%S',time.localtime(time.time()));
+hourcha = int(byebyetime.split(':')[0]) - int(nowTime.split(':')[0])
+mincha = int(byebyetime.split(':')[1]) - int(nowTime.split(':')[1])
+seccha = int(byebyetime.split(':')[2]) - int(nowTime.split(':')[2])
+print("h%s , m:%s  , s:%s"%(hourcha,mincha,seccha))
+shutdownsec = ((hourcha*60)+mincha)*60+seccha
 
-hourcha = int(Closingtime.split(':')[0]) - int(nowTime.split(':')[0]);
-mincha = int(Closingtime.split(':')[1]) - int(nowTime.split(':')[1]);
-seccha = int(Closingtime.split(':')[2]) - int(nowTime.split(':')[2]);
-print("h%s , m:%s  , s:%s"%(hourcha,mincha,seccha));
-shutdownsec = ((hourcha*60)+mincha)*60+seccha;
-
-str_cmd = "shutdown -s -t %s"%(shutdownsec);
-print(str_cmd);
-os.system(str_cmd);
+str_cmd = "shutdown -s -t %s"%(shutdownsec)
+print(str_cmd)
+os.system(str_cmd)
