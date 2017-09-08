197b2e5ccca2ee9c14d3085b132f8ba19fc4e7e1
knva <txt_ch@163.com>
2017-09-08 09:44:46 +0800

    判断星期五

--- shutdown.py
+++ shutdown.py
@@ -16,6 +16,9 @@ if(int(startHour)<9 and int(startMin)<40):
     byebyetime = "18:00:00"
 else:
     byebyetime = "18:30:00"
+
+if(datetime.datetime.now().weekday()==4)
+    byebyetime = "16:30:00"
 nowTime =  time.strftime('%H:%M:%S',time.localtime(time.time()))
 
 hourcha = int(byebyetime.split(':')[0]) - int(nowTime.split(':')[0])
@@ -23,7 +26,8 @@ mincha = int(byebyetime.split(':')[1]) - int(nowTime.split(':')[1])
 seccha = int(byebyetime.split(':')[2]) - int(nowTime.split(':')[2])
 print("h%s , m:%s  , s:%s"%(hourcha,mincha,seccha))
 shutdownsec = ((hourcha*60)+mincha)*60+seccha
-
+cancelCmd = "shutdown -a"
 str_cmd = "shutdown -s -t %s"%(shutdownsec)
 print(str_cmd)
+os.system(cancelCmd)
 os.system(str_cmd)
